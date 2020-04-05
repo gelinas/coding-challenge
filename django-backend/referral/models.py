3ffad0a1175e79f5b8eed5f0060b5ce6ca0e1c13
gelinas <jlgelinas@gmail.com>
2020-04-05 18:22:38 -0400

    create datamodel, add to admin, seed data

--- django-backend/referral/models.py
+++ django-backend/referral/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    name = models.CharField(max_length=255, default="Landing page name")
+    owner = models.CharField(max_length=255, default="Page owner")
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class Referrer(models.Model):
+    name = models.CharField(max_length=50, default="Referrer name")
+    count = models.IntegerField(default=0)
+    page = models.ForeignKey(
+        'Page',
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
